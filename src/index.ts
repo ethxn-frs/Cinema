deb04c449a203583c6318713a130bae38387b54f
ADJAKPA HECTOR <hector@pc-34.home>
2024-04-10 00:27:24 +0200

    premier commit, ajout des reoutes coté user et les validator coté user

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { initRoutes } from "./handlers/routes";
+import { AppDataSource } from "./database/database";
+
+
+const main = async () => {
+    const app = express()
+    const port = 8088
+    try {
+
+        await AppDataSource.initialize()
+        console.error("well connected to database")
+    } catch (error) {
+        console.log(error)
+        console.error("Cannot contact database")
+        process.exit(1)
+    }
+
+    app.use(express.json())
+    initRoutes(app)
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`)
+    })
+}
+
+main()
